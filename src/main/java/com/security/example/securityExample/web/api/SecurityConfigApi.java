c01e725df7d5b54713430881e88a202e5a64f346
yoojin Lee <leeujin1029@naver.com>
2022-03-25 14:32:33 +0900

    Feat: 테스트용 rest메서드 추가
    일반로그인과 OAuth2 로그인의 차이점을 알아보기 위한 테스트 메서드 생성

--- src/main/java/com/security/example/securityExample/web/api/SecurityConfigApi.java
+++ src/main/java/com/security/example/securityExample/web/api/SecurityConfigApi.java
@@ -1,12 +1,19 @@
 package com.security.example.securityExample.web.api;
 
+import com.security.example.securityExample.config.auth.PrincipalDetails;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class SecurityConfigApi {
     @GetMapping("/manager")
@@ -33,4 +40,27 @@ public class SecurityConfigApi {
     @GetMapping("/data")
     public
     String data() { return "data"; }
+
+    @GetMapping("/test/login")
+    public String testLogin(Authentication authentication, /*로그인 정보를 가져오는 방법*/
+                            @AuthenticationPrincipal PrincipalDetails/*or UserDetails*/ userDetails) {
+
+        log.info((String) authentication.getPrincipal());
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+        log.info("authentication: " + principalDetails.getUser().getUsername());
+        log.info("userDetailsPrincipal: " + userDetails.getUsername());
+
+        return "세션정보 확인";
+    }
+
+    @GetMapping("/test/oauth/login")
+    public String testOAuthLogin(Authentication authentication,
+                                 @AuthenticationPrincipal OAuth2User oAuth) {
+
+        log.info(String.valueOf(authentication.getPrincipal()));
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        log.info("authentication: " + oAuth2User.getAttributes());
+        log.info("oAuth: " + oAuth.getAttributes());
+        return "oAuth 세션정보 확인";
+    }
 }
