266ed6f56a79338f8492ca62ac63c19193fd8294
yoojin Lee <leeujin1029@naver.com>
2022-03-25 16:35:57 +0900

    Feat: 생성자 추가 및 OAuth2User 상속
    Authentication은 UserDetail, OAuth2User만 담을 수 있다.

--- src/main/java/com/security/example/securityExample/config/auth/PrincipalDetails.java
+++ src/main/java/com/security/example/securityExample/config/auth/PrincipalDetails.java
@@ -1,11 +1,14 @@
 package com.security.example.securityExample.config.auth;
 
 import com.security.example.securityExample.web.domain.User;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 /**
  * 시큐리티가 /login 주소 요청이 오면 낚아채서 로그인을 진행
@@ -14,12 +17,22 @@ import java.util.Collection;
  * User 오브젝트 타입 => UserDetails 타입 객체
  * Security Session => Authentication => UserDetails
  * */
-public class PrincipalDetails implements UserDetails {
 
+
+@Getter
+public class PrincipalDetails implements UserDetails, OAuth2User /*Authentication 객체는 UserDetails, OAuth2User로 다운캐스팅 가능하므로 두 가지를 Impl화 시켜준다*/ {
     private User user;
+    private Map<String, Object> attributes;
 
+    //일반 로그인
     public PrincipalDetails(User user) {
         this.user = user;
+    }
+
+    //OAuth2 로그인
+    public PrincipalDetails(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
     }
 
     //해당 유저의 권한을 리턴
@@ -65,4 +78,16 @@ public class PrincipalDetails implements UserDetails {
 
         return true;
     }
+
+    /*OAuth2User의 메서드*/
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+    /***/
 }
